2b5a62cbc681a37d820d85cf4f60fc6aa73af33d
Jonas Cosandey <jonas.cosandey@redcor.ch>
2016-02-10 16:35:01 +0100

    Add action listener

--- Open Mouse Configurator/src/Main.java
+++ Open Mouse Configurator/src/Main.java
@@ -1,23 +1,53 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 
 public class Main {
 
 	private Profile[] profiles;
+	private final JFrame frame = new JFrame();
 
 	public static void main(String[] args) {
 
 		Main main = new Main();
+		main.generateProfiles();
 
+		main.frame.setSize(100, 500);
+		main.frame.setResizable(false);
+
+		JComboBox c = new JComboBox();
+		for (int i = 0; i < main.profiles.length; i++) {
+			c.addItem(main.profiles[i].getName());
+			c.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					System.out.println("changed");
+				}
+			});
+		}
+		c.setEditable(true);
+		main.frame.add(c);
+
+		main.frame.setVisible(true);
+
+	}
+
+	private void generateProfiles() {
 		Reader unformattedProfiles = new Reader(System.getProperty("user.home") + "/.omc_profiles");
 		String[] data = unformattedProfiles.data.split("\n");
-		
-		main.profiles = new Profile[data.length];
+
+		profiles = new Profile[data.length];
 		for (int i = 0; i < data.length; i++) {
 			String[] splittedData = data[i].split(";");
-			String[] buttons = new String[splittedData.length-1];
+			String[] buttons = new String[splittedData.length - 1];
 			for (int j = 1; j < splittedData.length; j++) {
-				buttons[j-1] = splittedData[j];
+				buttons[j - 1] = splittedData[j];
 			}
-			main.profiles[i] = new Profile(splittedData[0], buttons);
+			profiles[i] = new Profile(splittedData[0], buttons);
 		}
+
 	}
 }
